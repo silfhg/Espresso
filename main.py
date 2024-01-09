502d4703e8830b5bd9d61a0b740710635a434326
silfhg <151051387+silfhg@users.noreply.github.com>
2024-01-09 23:14:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
- import sqlite3
-from sqlite3 import Error
-w=12                      
-database = "C:\\sqlite\pythonsqlite.db"
-conn = sqlite3.connect(database)
-cur = conn.cursor()
-cur.execute("SELECT * FROM users")
-col_name_list = [t[0] for t in cur.description]
-print(*[s.ljust(w) for s in col_name_list])
-print('-'*w*3)          
-rows = cur.fetchall()
-for row in rows:
-        print(*[str(s).ljust(w) for s in row])
-cur.close()
-conn.close()
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtWidgets import QApplication
+
+from window import Window
+
+import sys
+
+
+if __name__ == "__main__":
+    app: QApplication(sys.argv)
+    window: Window = Window()
+    window.show()
+    sys.exit(app.exec())
+ 
